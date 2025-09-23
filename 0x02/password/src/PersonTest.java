bf287941a7653992ffab307024f1edd07a7b6f34
EvertonMendioli <evertonbaixista@gmail.com>
2025-09-23 12:59:57 +0000

    ajustes

--- 0x02/password/src/PersonTest.java
+++ 0x02/password/src/PersonTest.java
@@ -1,5 +1,10 @@
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 public class PersonTest {
 
     static Person person;
